c265bdd577abc47adc5c971dc3ab73cd3a3ae051
Zoran Milosavljevic <zomilo@hotmail.com>
2022-02-01 17:00:39 -0500

    Added Lab 3, and Lab 3 Test 1

--- Game 1001/Lab2.cpp
+++ Game 1001/Lab2.cpp
@@ -1,39 +1,39 @@
-// Intro to Programming 1001 - Lab 2
-
-/*
-Lab 2 Instructions:
-- Declare 3 variables that will each hold a number (with decimal places).
-- Call the variables number1, number2, number3.
-- Ask the user to enter a number of their choice for each declared variable.
-- Calculate the sum, product and average of the three variables and display the results to the user.
-- Submit one .cpp (Lab2.cpp) to the dropbox. (Github repository link also acceptable)
-*/
-
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    //This section of code is prompting the user to enter their chosen numbers.
-    //Important to note that the variables below are all listed as doubles - because they are decimals - to avoid demotions.
-    double number1, number2, number3, sum, product, average;
-    cout << "Please enter a number of your choice for your 1st number: ";
-    cin >> number1;
-    cout << "Please enter a number of your choice for your 2nd number: ";
-    cin >> number2;
-    cout << "Please enter a number of your choice for your 3rd number: ";
-    cin >> number3;
-    cout << "\n";
-
-    //This is calculating the numbers the user has entered above.
-    sum = number1 + number2 + number3;
-    product = number1 * number2 * number3;
-    average = (number1 + number2 + number3) / 3;
-
-    //Lists the sum, product, and average of said numbers the user has entered.
-    cout << "The sum of " << number1 << ", " << number2 << " and " << number3 << " is: " << sum << "\n";
-    cout << "The product of " << number1 << ", " << number2 << " and " << number3 << " is: " << product << "\n";
-    cout << "The average of " << number1 << ", " << number2 << " and " << number3 << " is: " << average << "\n";
-
-    return 0;
-}
+//// Intro to Programming 1001 - Lab 2
+//
+///*
+//Lab 2 Instructions:
+//- Declare 3 variables that will each hold a number (with decimal places).
+//- Call the variables number1, number2, number3.
+//- Ask the user to enter a number of their choice for each declared variable.
+//- Calculate the sum, product and average of the three variables and display the results to the user.
+//- Submit one .cpp (Lab2.cpp) to the dropbox. (Github repository link also acceptable)
+//*/
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//    //This section of code is prompting the user to enter their chosen numbers.
+//    //Important to note that the variables below are all listed as doubles - because they are decimals - to avoid demotions.
+//    double number1, number2, number3, sum, product, average;
+//    cout << "Please enter a number of your choice for your 1st number: ";
+//    cin >> number1;
+//    cout << "Please enter a number of your choice for your 2nd number: ";
+//    cin >> number2;
+//    cout << "Please enter a number of your choice for your 3rd number: ";
+//    cin >> number3;
+//    cout << "\n";
+//
+//    //This is calculating the numbers the user has entered above.
+//    sum = number1 + number2 + number3;
+//    product = number1 * number2 * number3;
+//    average = (number1 + number2 + number3) / 3;
+//
+//    //Lists the sum, product, and average of said numbers the user has entered.
+//    cout << "The sum of " << number1 << ", " << number2 << " and " << number3 << " is: " << sum << "\n";
+//    cout << "The product of " << number1 << ", " << number2 << " and " << number3 << " is: " << product << "\n";
+//    cout << "The average of " << number1 << ", " << number2 << " and " << number3 << " is: " << average << "\n";
+//
+//    return 0;
+//}
---
+++ Week 4/Lab 3.cpp
@@ -0,0 +1,25 @@
+// Game 1001 - Intro to Programming
+// Lab 3
+
+// Instructions are as follows:
+
+/*
+
+Create two variables called "hoursWorked"and "hourlyRate".
+
+Ask the user how many hours they worked in the week and their hourly rate. Populate the variables with necessary information.
+
+Determine whether the user requires overtime pay. Overtime is typically applied when working greater than 40 hours per week.
+
+Calculate total pay.
+   - Regular pay is the number of hours worked multiplied by the hourly rate.
+
+Overtime pay includes regular pay plus any hours exceeding 40 multiplied by “time and a half” or 1.5. 
+(Example: If I work 50 hours in the week, 40 hours will be regular pay and 10 hours will be overtime pay at 1.5 times the hourly rate..
+
+Display regular pay.
+Display overtime pay.
+Display total pay.
+
+*/
+

4aa245c8551b4d171cd1da3be0303645a0f0aff5
Zoran Milosavljevic <zomilo@hotmail.com>
2022-02-18 05:45:55 -0500

    Completed Lab 5

--- Week 6/Lab 5.cpp
+++ Week 6/Lab 5.cpp
@@ -1,24 +1,5 @@
 // Lab 5 - Arrays & Vectors - Intro to Programming GAME1001
 
-/*
-Instructions:
-Write a C++ console application that will split an array into two vectors.
-	
-	1. Create an array that hold 10 integers.
-	
-	2. Allow the user to enter integers into the newly created array.
-	
-	3. Determine what the average number is of the array.
-	
-	4. Create two integer vectors. One vector will hold numbers less than or equal to the average number. 
-	The other vector will hold numbers greater than the average number.
-	
-	5. Using the average number, split your array into the two vectors.
-	
-	6. Display both vectors to the console.
-
-Submit a single .cpp file to the dropbox.
-*/
 
 #include <iostream>
 #include <iomanip>
@@ -34,6 +15,8 @@ int main()
     float average;
     int arraySum = 0;
     int i;
+    vector<int> lessOrEqualToAvg;
+    vector<int> greaterToAvg;
 
     cout << "Please enter " << NUM_INTEGERS << " random integers of your choosing.\n";
     for (i = 0; i < NUM_INTEGERS; i++)
@@ -42,29 +25,45 @@ int main()
         arraySum += digitsEntered[i];
     }
 
-    cout << "The sum of your" << NUM_INTEGERS << " digits entered is : " << arraySum << endl;
+    cout << "The sum of your " << NUM_INTEGERS << " digits entered is : " << arraySum << endl;
 
     average = arraySum / NUM_INTEGERS;
-    cout << "The average of your" << NUM_INTEGERS << " digits entered is : " << average << endl;
+    cout << "The average of your " << NUM_INTEGERS << " digits entered is : " << average << endl;
     
-    cout << "These are the digits that are less or equal to the average: " << average << endl;
     i = 0;
     for(i = 0; i < 10; i++)
     {
         if (digitsEntered[i] <= average)
         {
-            cout << digitsEntered[i] << endl;
+            lessOrEqualToAvg.push_back(digitsEntered[i]);
         }
     }
    
-    cout << "These are the digits that are greater than the average: " << average << endl;
     i = 0;
     for (i = 0; i < 10; i++)
     {
         if (digitsEntered[i] > average)
         {
-            cout << (digitsEntered[i]) << endl;
+            greaterToAvg.push_back(digitsEntered[i]);
         }
+    }
+
+    cout << "The vector that contains all digits less or equal to the average of " << average << " displays the following:" << endl;
+
+    i = 0;
+    while (i < lessOrEqualToAvg.size())
+    {
+        cout << lessOrEqualToAvg[i] << endl;
+        i++;
+    }
+
+    cout << "The vector that contains all digits greater than the average of " << average << " displays the following:" << endl;
+
+    i = 0;
+    while (i < greaterToAvg.size())
+    {
+        cout << greaterToAvg[i] << endl;
+        i++;
     }
 
     return 0;
